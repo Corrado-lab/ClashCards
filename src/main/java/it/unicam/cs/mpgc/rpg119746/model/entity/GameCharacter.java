df2c2b72c4c76bfebe51e77ef280a07e25e1f414
Corrado-lab <corrado.cecchini@studenti.unicam.it>
2026-07-01 11:08:46 +0200

    feat(doc): add methods in GameCharacter class

--- src/main/java/it/unicam/cs/mpgc/rpg119746/model/entity/GameCharacter.java
+++ src/main/java/it/unicam/cs/mpgc/rpg119746/model/entity/GameCharacter.java
@@ -24,5 +24,36 @@ public abstract class GameCharacter {
     public int getMaxHealthPoints() { return maxHealthPoints; }
     public int getBaseDamage() { return baseDamage; }
 
+    public boolean isAlive() { return this.isAlive; }
+
+    public void attack(GameCharacter target) {
+
+        if (target == null) {
+            throw new NullPointerException("Il bersaglio dell'attacco non può essere null.");
+        }
+        if (!this.isAlive()) {
+            throw new IllegalStateException(this.name + " è morto e non può attaccare!");
+        }
+        if (!target.isAlive()) {
+            throw new IllegalStateException("Non puoi attaccare " + target.getName() + " perché è già morto!");
+        }
+
+        target.takeDamage(this.baseDamage);
+    }
+
+    public void takeDamage(int damage) {
+        if (damage < 0) {
+            throw new IllegalArgumentException("Il danno non può essere negativo! Danno inserito: " + damage);
+        }
+        
+        this.healthPoints -= damage;
+        if (this.healthPoints <= 0) {
+            this.healthPoints = 0;
+            this.isAlive = false;
+        }
+    }
+    
+    public abstract void useAbility(GameCharacter target);
+
 
 }
