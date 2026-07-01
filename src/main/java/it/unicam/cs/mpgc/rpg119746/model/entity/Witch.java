16a0ddf05380a6985bf39b6f999c8610bc014997
Corrado-lab <corrado.cecchini@studenti.unicam.it>
2026-07-01 18:03:47 +0200

    feat(doc): add methods in Witch class

--- src/main/java/it/unicam/cs/mpgc/rpg119746/model/entity/Witch.java
+++ src/main/java/it/unicam/cs/mpgc/rpg119746/model/entity/Witch.java
@@ -2,9 +2,41 @@ package it.unicam.cs.mpgc.rpg119746.model.entity;
 
 public class Witch extends Enemy{
 
+    private boolean hasUsedAbility = false;
+
     public Witch (){
 
-        super("Witch", 100, 40, 80);
+        super("Witch", 100, 20, 80);
     }
 
+    @Override
+    public void useAbility(GameCharacter target) {
+        if (!this.isAlive()) {
+            throw new IllegalStateException("La Strega è stata distrutta!");
+        }
+        
+        this.heal(40);
+        
+        int newDamage = Math.max(0, this.getBaseDamage() - 10);
+        this.setBaseDamage(newDamage);
+        
+        this.hasUsedAbility = true;
+    }
+
+    @Override
+    public void takeTurn(GameCharacter player) {
+        
+        
+        if (this.getHealthPoints() <= 40 && !this.hasUsedAbility) {
+            
+            this.useAbility(player);
+            this.attack(player);     
+            
+        } else {
+
+            this.attack(player);
+        }
+    }
 }
+
+
