6e0f00892076d932851f8fbe156b0d566e6b7546
Corrado-lab <corrado.cecchini@studenti.unicam.it>
2026-07-02 10:38:32 +0200

    feat(doc): add methods in Yeti class

--- src/main/java/it/unicam/cs/mpgc/rpg119746/model/entity/Yeti.java
+++ src/main/java/it/unicam/cs/mpgc/rpg119746/model/entity/Yeti.java
@@ -7,4 +7,32 @@ public class Yeti extends Enemy{
         super("Yeti", 150, 30, 70);
 
     }
+
+    @Override
+    public void useAbility(GameCharacter target) {
+        if (!this.isAlive()) {
+            throw new IllegalStateException("Lo Yeti è stato distrutto!");
+        }
+        
+        super.attack(target);
+        super.attack(target);
+    }
+
+    @Override
+    public void takeTurn(GameCharacter player) {
+        
+        
+        double probabilita = Math.random();
+        
+        if (probabilita <= 0.30) {
+            
+            this.useAbility(player); 
+            
+        } else {
+            
+            this.attack(player);     
+            
+        }
+    }
 }
+
