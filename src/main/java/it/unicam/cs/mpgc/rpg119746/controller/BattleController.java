579123793ded5788477bf8b75da9794087db9338
Corrado-lab <corrado.cecchini@studenti.unicam.it>
2026-07-03 11:22:25 +0200

    feat(doc): add methods in BattleController class

--- src/main/java/it/unicam/cs/mpgc/rpg119746/controller/BattleController.java
+++ src/main/java/it/unicam/cs/mpgc/rpg119746/controller/BattleController.java
@@ -24,5 +24,56 @@ public class BattleController {
     public Enemy getEnemy() { return enemy; }
     public boolean isPlayerTurn() { return isPlayerTurn; }
 
+
+    public void executePlayerAttack() {
+        if (!isPlayerTurn || !player.isAlive() || !enemy.isAlive()) return;
+
+        player.attack(enemy);
+        endPlayerTurn();
+    }
+
+    
+    public void executePlayerAbility() {
+        if (!isPlayerTurn || !player.isAlive() || !enemy.isAlive()) return;
+
+        player.useAbility(enemy);
+        System.out.println("Il Warden infliggerà danni doppi!");
+        
+    }
+
+    
+    private void endPlayerTurn() {
+        if (!enemy.isAlive()) {
+            handleVictory();
+        } else {
+
+            this.isPlayerTurn = false;
+            executeEnemyTurn();
+        }
+    }
+
+    
+    private void executeEnemyTurn() {
+        
+        enemy.takeTurn(player);
+        
+        if (!player.isAlive()) {
+            handleDefeat();
+        } else {
+
+            this.isPlayerTurn = true;
+        }
+    }
+
+    
+    private void handleVictory() {
+        System.out.println("\n Bene! Hai sconfitto " + enemy.getName() + "!");
+        player.gainExperience(enemy.getExpReward());
+    }
+
+    
+    private void handleDefeat() {
+        System.out.println("\n Il Warden è è stato sconfitto, hai perso.");
+    }
     
 }
